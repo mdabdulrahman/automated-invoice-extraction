81bbd439d41bab74fbac0a86cb52fdc92c6cd2fc
Abdul Rahman <abdulrahmanasus@outlook.com>
2024-11-23 22:27:44 +0530

    decreased text size

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,11 +4,10 @@ function Footer() {
   return (
     <div>
       <div className="bg-black text-center p-2 fixed rounded-sm right-0 bottom-0">
-        <p className="text-white font-medium">
+        <p className="text-white text-sm font-medium">
           Created by{" "}
           <a href="https://linkedin.com/in/smdabdulrahman" target="_blank">
-            {" "}
-            Mohamed Abdul Rahman{" "}
+            Mohamed Abdul Rahman
           </a>
         </p>
       </div>
